5d08b68bc19840dfd1873f8eb4ad592f9b328990
pr3n3y <pranay.ambre@torero.in>
2025-01-15 15:31:36 +0530

    UPDATE

--- src/components/productListing/ProductPage.jsx
+++ src/components/productListing/ProductPage.jsx
@@ -49,33 +49,12 @@ const ProductListing = ({
   return (
     <div className="max-w-7xl mx-auto md:p-4 md:pl-0 ">
       <div className="p-3 mx-4 md:mx-0 pb-0 rounded-lg bg-white">
-        <div className="md:flex hidden items-center mb-4">
-          <h1 className="text-2xl font-semibold capitalize">
+        <div className="md:flex hidden items-center mb-4 gap-2">
+          <h1 className="text-2xl font-semibold capitalize">{title}</h1>|
+          <span className="flex items-center gap-4 text-gray-600 text-md font-md  capitalize">
+            {products.length ?? 0} of {hasProducts ? products.length : 0}{" "}
             {title}
-            {"    "}
-          </h1>
-          <div className="flex items-center gap-4">
-            {/* <span className="bg-[#008539] text-white px-2 py-0 rounded-md flex items-center ml-2 text-sm">
-              {rating}{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="ml-1 size-4"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span> */}
-            {/* <span className="text-blue-600 mr-2">{reviewCount} Reviews</span> */}
-            <span className="text-gray-600 text-md font-md  capitalize">
-              | {products.length ?? 0} of {hasProducts ? products.length : 0}{" "}
-              {title}
-            </span>
-          </div>
+          </span>
         </div>
 
         <div className="md:flex hidden space-x-4 mb-4">
